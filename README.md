f40ab15b5ac3a8e2e6298ea4dd65096a203d9c2b
Adam Smith <adamsmith@Adams-iMac.local>
2014-12-08 16:07:55 +0000

    updated readme

---
+++ README.md
@@ -0,0 +1,16 @@
+# shine.js
+
+Shine.js adds a "Slide to unlock" shine using WebKit/CSS3.
+
+**example**
+
+JavaScript
+
+```javascript
+$('h1').shine({ shineColor: 'rgb(149, 235, 255)', textColor: 'purple'});
+```
+
+HTML
+```html
+<h1>Hello World</h1>
+```
--- src/js/shine.js
+++ src/js/shine.js
@@ -4,7 +4,7 @@ $.fn.shine = function(options) {
 		var textColor = '#222';
 
 		if(options.shineColor) shineColour = options.shineColor
-		if(options.textColor) textColor = options.shineColor;
+		if(options.textColor) textColor = options.textColor;
 
 		var background = '-webkit-gradient(linear, left top, right top, from('+textColor+'), to('+textColor+'), color-stop(0.5, '+shineColour+')) 0 0 no-repeat';
 		this.css({background: background, 'background-color' : textColor, '-webkit-background-clip': 'text', '-webkit-background-size': '125px'});
